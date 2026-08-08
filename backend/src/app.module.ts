2b863661a5497a19831ef19b32a11e330b85dcf3
Shashwat Gupta <guptashashwat155@gmail.com>
2026-08-08 20:34:30 +0530

    feat(backend): setup mongoose and config module connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI as string),
+    AuthModule,
+    UsersModule,
+    TasksModule,
+    ProjectsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
